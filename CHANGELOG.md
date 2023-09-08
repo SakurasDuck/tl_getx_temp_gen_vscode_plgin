30d5b223faf7cf344aadbfe956d9980f12597cfc
PAWN <13147130713@163.com>
2023-09-08 16:35:52 +0800

    feat: 添加了创建时对项目的tl_getx的依赖检查

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,7 @@ All notable changes to the "getx-ext-temp-gen" extension will be documented in t
 ## [0.0.1]
 
 - Initial release of getx-ext-temp-gen, support page(component) and list(list-component) template
+
+## [0.0.2]
+
+- 添加了对tl_getx的依赖检查,以及创建模块时的帕斯卡命名检查
--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,10 @@
 import * as vscode from 'vscode';
-import { dictExist, isPascalCase } from './utils';
+import { dictExist, fileExist, isPascalCase } from './utils';
 import { generateComponent, generateList, hump2Underline } from './generator';
 
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "getx-ext-temp-gen" is now active!');
-
 
 	context.subscriptions.push(vscode.commands.registerCommand(
 		"getx-ext-temp-gen.temp-page", async (args) => {
@@ -38,31 +37,91 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() { }
 
 
-async function gen(psiPath: string, generator: Function, needBinding: boolean) {
-	let text = await vscode.window.showInputBox({
-		placeHolder: "输入模块名称,例如TestPage"
+//检查当前文件夹即父文件夹是否存在pubspec.lock文件,且其中包含tl_getx的依赖
+async function checkPubspec(fs: vscode.FileSystem, psiPath: string): Promise<boolean> {
+	//判断当前路径是否存在pubspec.lock文件
+	if (psiPath.charAt(psiPath.length - 1) === '/') {
+		psiPath = psiPath.substring(0, psiPath.length - 1);
 	}
-	);
-	if (text) {
-		if (isPascalCase(text)) {
-			let fs = vscode.workspace.fs;
-			let name = hump2Underline(text);
-			let dict = `${psiPath}/${name}`;
-			let exist = await dictExist(fs, dict);
-			console.log(dict);
-			if (exist) {
-				console.log('文件夹已存在');
-				vscode.window.showErrorMessage("文件夹已存在");
-			} else {
-				//创建文件夹
-				console.log('文件夹不存在');
-				await fs.createDirectory(vscode.Uri.parse(dict));
-				await generator(fs, psiPath, text, needBinding);
-				vscode.window.showInformationMessage("创建完成");
-			}
+	let pubspecLockPath = `${psiPath}/pubspec.lock`;
+	console.log(pubspecLockPath);
+	let exist = await fileExist(fs, pubspecLockPath);
+	if (exist) {
+		//存在pubspec.lock文件
+		//读取文件内容,判断是否存在tl_getx依赖
+		let content = await fs.readFile(vscode.Uri.parse(pubspecLockPath));
+		let str = content.toString();
+		if (str.indexOf('tl_getx:') !== -1) {
+			return true;
 		} else {
-			vscode.window.showErrorMessage("模块非帕斯卡命名");
+			vscode.window.showErrorMessage("当前项目未引入tl_getx依赖");
+			return false;
 		}
 
+	} else {
+		//不存在pubspec.lock文件
+		//向上找一级
+		let parentPath = _removeLastPath(psiPath);
+		console.log(parentPath);
+		if (parentPath) {
+			return await checkPubspec(fs, parentPath);
+		} else {
+			vscode.window.showErrorMessage("未找到pubspec.lock文件");
+			return false;
+		}
+	}
+
+	return false;
+}
+
+//将路径去除一级
+function _removeLastPath(path: string): string | undefined {
+	
+	if (path.charAt(path.length - 1) === '/') {
+		path = path.substring(0, path.length - 1);
+	}
+	if(path.charAt(path.length - 1) === ':'){
+		return undefined;
+	}
+
+	let index = path.lastIndexOf('/');
+	if (index === -1) {
+		return;
+	}
+	return path.substring(0, index + 1);
+}
+
+
+
+async function gen(psiPath: string, generator: Function, needBinding: boolean) {
+	let fs = await checkPubspec(vscode.workspace.fs, psiPath);
+	if (fs) {
+		let text = await vscode.window.showInputBox({
+			placeHolder: "输入模块名称,例如TestPage"
+		}
+		);
+		if (text) {
+			if (isPascalCase(text)) {
+				let fs = vscode.workspace.fs;
+				let name = hump2Underline(text);
+				let dict = `${psiPath}/${name}`;
+				let exist = await dictExist(fs, dict);
+				if (exist) {
+					console.log('文件夹已存在');
+					vscode.window.showErrorMessage("文件夹已存在");
+				} else {
+					//创建文件夹
+					await fs.createDirectory(vscode.Uri.parse(dict));
+					await generator(fs, psiPath, text, needBinding);
+					vscode.window.showInformationMessage("创建完成");
+				}
+			} else {
+				vscode.window.showErrorMessage("模块非帕斯卡命名");
+			}
+
+		}
 	}
 }
+
+
+export var removeLastPath = _removeLastPath;
